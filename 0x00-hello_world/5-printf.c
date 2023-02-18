757935b474ecb351478783c636df9ad3b91bc40f
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-02-18 07:06:47 +0000

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A C program that prints a statement with printf followed by a new line
+ *
+ * You are not allowed to use the function puts
+ * 
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char str[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", str);
+	return (0);
+}
