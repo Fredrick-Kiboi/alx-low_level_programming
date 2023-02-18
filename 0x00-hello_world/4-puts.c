4394e9235ae031f0bcf31e8cb996e68331e94e94
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-02-18 06:54:07 +0000

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * Main - A C program that prints a statement followed by a new line
+ *
+ * Use the function puts
+ * You are not allowed to use printf
+ *
+ * Return - Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle,";
+	puts(str);
+	return 0;
+}
