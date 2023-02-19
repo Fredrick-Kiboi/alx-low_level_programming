7a85f1afdcfbaa7b3373f1dfc71ca6d2e9bb0fbc
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-02-19 17:57:30 +0000

    print a statement to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	for (i = 0; i < 70; i++)
+	{
+		putc("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"[i], stderr);
+	}
+	return (1);
+}
