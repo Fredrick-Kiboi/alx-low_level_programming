002a872f6eead8209127dce6035a6f854cb8f019
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-02-21 08:47:43 +0000

    a program that prints to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main()
+/**
+ * main - program that prints a statement to standard error
+ *
+ * Return: always (1) if successful, otherwise appropriate errno
+ */
+
+int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(STDERR_FILENO, str, sizeof(str));
 	return (1);
 }
