923091a6b5e19fafd7b60bc9a5eeae4b43d7c82e
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-02-25 07:01:23 +0000

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and return the result
+ *
+ * Description : function that adds two integers
+ * Prototype: int add(int, int);
+ * @num1: first parameter
+ * @num2: second parameter
+ *
+ * Return: int(result of the sum of two integers)
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * times_table - prints a time table
+ *
+ * Description: function that prints the 9 times table
+ * it should start with 0
+ * 
+ * Prototype: void times_table(void);
+ *
+ * Return: Void
+ */
+
+void times_table(void)
+{
+	int row, colm, mul;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (colm = 0; colm <= 9; colm++)
+		{
+			mul = row * colm;
+			
+			if (mul <= 9 && row != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(mul + '0');
+				if (colm != 9)
+					_putchar(',');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar((mul / 10) + '0');
+			        _putchar((mul % 10) + '0');
+			}
+			
+			if (colm != 9)
+				_putchar(',');
+		}
+		_putchar('\n');
+	}	
+}
