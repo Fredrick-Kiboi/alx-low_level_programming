4caa078179d9f2291e6bc1e932ec508246117ecd
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-03-08 14:41:02 +0000

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,6 +4,8 @@
  * _pow_recursion - function that returns the value of x to the power of y
  *
  * Prototype: int _pow_recursion(int x, int y);
+ * @x: 1st parameter
+ * @y: 2nd parameter
  * if y is lower than 0, the function should return -1
  *
  * Return: integer (x to the power of y)
@@ -20,9 +22,9 @@ int _pow_recursion(int x, int y)
 		return (1);
 	}
 	else if (y == 1)
-		return x;
-	else
+		return (x);
+	else if (y > 1)
 	{
-		return x * _pow_recursion(x, y - 1);
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
