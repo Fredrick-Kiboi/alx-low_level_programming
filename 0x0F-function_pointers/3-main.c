ded205e54f47fabf773ae6e23b5008987a41c3ea
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-03-23 18:20:00 +0000

    completed

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,14 +3,13 @@
 #include <stdio.h>
 
 /**
- * main - prints the result of simple operations
- * @argc: the number of arguments supplied to the program
- * @argv: An array of pointers to the arguments
- * 
- * Return: Always 0
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
  */
-
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num2;
 	char *op;
@@ -30,11 +29,15 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*op == '\' && num2 == 0) || (*op == '%' && num2 == 0))
+
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
+
 	printf("%d\n", get_op_func(op)(num1, num2));
+
 	return (0);
 }
