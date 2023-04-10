0127c3142193bc74cbb478f1bb17fb154274d43e
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-04-10 15:25:52 +0000

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,7 +2,6 @@
 
 /**
  * isEven - checks if a number is even
- * 
  * Prototype: int isEven(int num);
  * @num: the number to check if it's even
  *
@@ -22,21 +21,6 @@ int isEven(int num)
 }
 
 /**
- * printer - a function that prints
- *
- * @num: the number to print
- * Return: an integer
- */
-
-int printer(long int num)
-{
-    char buf[20];
-    int len = snprintf(buf, 20, "%ld", num);
-    write(1, buf, len);
-    return (0);
-}
-
-/**
  * print_binary - function that prints the binary representation of a number
  *
  * Prototype: void print_binary(unsigned long int n);
@@ -45,12 +29,24 @@ int printer(long int num)
  * Return: nothing
  */
 
-int for_print_binary(unsigned long int n)
+void print_binary(unsigned long int n)
 {
-	long int i = 0, j = 0, final = 0, other_digits, exp, rightmost, digit;
-	exp = 1;
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		if (n == 1)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			int b = isEven(n);
 
-	rightmost = isEven(n);
-	:
+			print_binary(n >> 1);
+			_putchar(b + '0');
+		}
+	}
 }
-
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int _putchar(char c);
 int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 unsigned int binary_to_uint(const char *b);
