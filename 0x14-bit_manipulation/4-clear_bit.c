6b7b455e8632debb563326af4c73bdf8a3b8c735
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-04-11 20:02:48 +0000

    set bit to 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -15,6 +15,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 	place = ~(1 << index);
-	*n = *n & set;
+	*n = *n & place;
 	return (1);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,6 +6,7 @@
 #include <unistd.h>
 
 int _putchar(char c);
+int clear_bit(unsigned long int *n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 unsigned int binary_to_uint(const char *b);
