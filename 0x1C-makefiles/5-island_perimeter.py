393202e17316c74e7864bf2a820936fa029d5dca
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-07-27 02:56:40 +0000

    Island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,16 +13,18 @@ def island_perimeter(grid: list):
     Returns:
         int: Perimeter of island
     """
-    rows = len(grid)
-    perimeter = 0
-    for i in range(rows):
-        columns = len(grid[i])
-        for j in range(columns):
-            if (grid[i][j] == 1):
-                left = 1 if j == 0 or grid[i][j - 1] == 0 else 0
-                top = 1 if i == 0 or grid[i - 1][j] == 0 else 0
-                right = 1 if j == columns - 1 or grid[i][j + 1] == 0 else 0
-                bottom = 1 if i == rows - 1 or grid[i + 1][j] == 0 else 0
-                perimeter += (top + right + bottom + left)
-        return perimeter
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
 
